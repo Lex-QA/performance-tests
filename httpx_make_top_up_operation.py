edbb2802d1281843310b8a801063697e3e63a16c
Lex-QA <dev4lex@gmail.com>
2026-03-10 23:23:24 +0300

    Практикуемся в работе с HTTPX

--- httpx_make_top_up_operation.py
+++ httpx_make_top_up_operation.py
@@ -21,16 +21,17 @@ open_debit_card_account_response = httpx.post("http://localhost:8003/api/v1/acco
                                               json=open_debit_card_account_payload)
 open_debit_card_account_response_data = open_debit_card_account_response.json()
 
-issue_virtual_card_payload = {
+make_top_up_operation_payload = {
 
-    "userId": f"{create_user_response_data['user']['id']}",
-    "accountId": f"{open_debit_card_account_response_data['account']['id']}"
+    "status": "COMPLETED",
+    "amount": 1500,
+    "cardId": open_debit_card_account_response_data['account']['cards'][0]['id'],
+    "accountId": open_debit_card_account_response_data['account']['id']
 
 }
+make_top_up_operation_response = httpx.post("http://localhost:8003/api/v1/operations/make-top-up-operation",
+                                            json=make_top_up_operation_payload)
+make_top_up_operation_response_data = make_top_up_operation_response.json()
 
-issue_virtual_card_response = httpx.post("http://localhost:8003/api/v1/cards/issue-virtual-card",
-                                         json=issue_virtual_card_payload)
-issue_virtual_card_response_data = issue_virtual_card_response.json()
-
-print("Issue virtual card response:", issue_virtual_card_response_data)
-print("Issue virtual card status code:", issue_virtual_card_response.status_code)
+print("Make top up operation response:", make_top_up_operation_response_data)
+print("Make top up operation status code:", make_top_up_operation_response.status_code)
