cc60fdb65098034bca9503275d9acbebfdf6ca09
Lex-QA <dev4lex@gmail.com>
2026-03-11 22:27:59 +0300

    Знакомство с Pydantic

---
+++ pydantic_create_user.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    """
+    Модель данных пользователя.
+    """
+    id: str
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserRequestSchema(BaseModel):
+    """
+    Модель запроса на создание пользователя.
+    """
+    email: EmailStr
+    last_name: str = Field(alias="lastName")
+    first_name: str = Field(alias="firstName")
+    middle_name: str = Field(alias="middleName")
+    phone_number: str = Field(alias="phoneNumber")
+
+
+class CreateUserResponseSchema(BaseModel):
+    """
+    Модель ответа с данными созданного пользователя.
+    """
+    user: UserSchema
